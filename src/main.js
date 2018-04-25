88cc23784ce00c8413d3d24bc7c30983f24e9685
lipenghui <abc@163.com>
2018-04-25 18:27:21 +0800

    项目全局CSS文件配置

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
+import fastclick from 'fastclick'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/styles/index.css'
+fastclick.attach(document.body)
 Vue.config.productionTip = true
 
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
